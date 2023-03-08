3371c95d76331f8919dc96bd23165e122553e70f
Jason Nguyen <jason@fa:ee:e8:86:8f:08.vc.shawcable.net>
2023-03-08 01:25:54 -0800

    refactor layout and start green side panel

--- client/src/pages/Connections/Connections.jsx
+++ client/src/pages/Connections/Connections.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Connections() {
+    let location = useLocation();
+    console.log(location);
     return (
         <h1>Connections page goes here</h1>
     )
