10140c62b04a31cb136d7b180861c6ff0f8cf31a
nathanlao <gla93@sfu.ca>
2023-03-14 12:19:37 -0700

    Added route for login and use LoginRequired component to protect the main content

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Home from './pages/Home/Home'
 import Connections from './pages/Connections/Connections'
 import Groups from './pages/Groups/Groups'
 import Setting from './pages/Setting/Setting'
+import Login from './pages/Login'
+import LoginRequired from './components/LoginRequired'
 
 import './App.css'
 
@@ -14,11 +16,14 @@ export default function App() {
         <div>
             <main className='main'>
                 <Routes>
-                    <Route path='/' element={<MainLayout />}>
-                        <Route index element={<Home />} />
-                        <Route path='connections' element={<Connections />} />
-                        <Route path='groups' element={<Groups />} />
-                        <Route path='setting' element={<Setting />} />
+                    <Route path='login' element={<Login />}/>
+                    <Route element={<LoginRequired />}>    
+                        <Route path='/' element={<MainLayout />}>
+                            <Route index element={<Home />} />
+                            <Route path='connections' element={<Connections />} />
+                            <Route path='groups' element={<Groups />} />
+                            <Route path='setting' element={<Setting />} />
+                        </Route>
                     </Route>
                 </Routes>
             </main>
