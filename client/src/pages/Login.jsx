4ec64473328e2423b38fe71d8d16eb46ca00a61a
nathanlao <gla93@sfu.ca>
2023-03-14 12:36:41 -0700

    Added 2 inputs and a button to handle login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,46 @@
-import React from "react";
-
+import React, { useState } from "react";
         
 export default function Login() {
+    const [loginFormData, setLoginFormData] = useState({
+        username: "",
+        password: ""
+    })
+
+    function handleInputChange(event) {
+        const { name, value } = event.target
+        setLoginFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault()
+
+        //TODO: GET real data from server and proceed to main content
+        console.log(loginFormData)
+    }
+
     return (
-        <h1>Login</h1>
+        <div>
+            <h1>Log in to your account</h1>
+            <form onSubmit={handleFormSubmit}>
+                <input 
+                    type="text" 
+                    name="username"
+                    placeholder="Username"
+                    value={loginFormData.username}
+                    onChange={handleInputChange}    
+                />
+                <input 
+                    type="password" 
+                    name="password"
+                    placeholder="Password"
+                    value={loginFormData.password}
+                    onChange={handleInputChange}
+                />
+                <button>Log in</button>
+            </form>
+        </div>
     )
 }
