d81a8b3de6e994f22419aeab1061229333375627
Risa Kawagoe <rkawagoe@sfu.ca>
2023-03-17 16:36:07 -0700

    cleaned up uneccessary code and files

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import './Login.css'
 
 export default function Login() {
+    const navigate = useNavigate()
+    const [loginFormData, setLoginFormData] = useState({
+        username: "",
+        password: ""
+    })
+
+    function handleInputChange(event) {
+        const { name, value } = event.target
+        setLoginFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault()
+
+        //TODO: GET real data from server and proceed to main content
+        console.log(loginFormData)
+        localStorage.setItem("login", true)
+
+        // make http POST request to http://localhost:3000
+        // handle response from server
+        // if response returns login status SUCCESS then navigate to the /
+        // if response returns login status INTERNALERR then display message 'Something went wrong with the server. Please try again later'
+        // else display error message 'Incorrect username or password'
+        navigate("/", {replace: true})
+    }
+
+    // NOTE: ABOVE FUNCTIONS ARE STILL INCOMPLETE
+
     return (
         <>
             <p>
