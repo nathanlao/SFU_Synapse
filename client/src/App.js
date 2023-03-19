9f75c59a503b93c57cdb6fbbd9a62c85f525a0ea
nathanlao <gla93@sfu.ca>
2023-03-18 21:42:13 -0700

    Added nested route for connection and groups

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Connections from './pages/Connections/Connections'
 import Groups from './pages/Groups/Groups'
 import Setting from './pages/Setting/Setting'
 import LoginSignupWindow from './components/LoginSignupWindow/LoginSignupWindow'
+import ChatWindow from './pages/ChatSubtab/ChatSubtab'
 
 import './App.css'
 
@@ -19,8 +20,15 @@ export default function App() {
                     <Route path='/signup' element={<LoginSignupWindow />} />
                     <Route path='/' element={<MainLayout />}>
                         <Route index element={<Home />} />
-                        <Route path='connections' element={<Connections />} />
-                        <Route path='groups' element={<Groups />} />
+                        <Route path='connections' element={<Connections />} >
+                            {/* Nested route relative to "/connection" */}
+                            <Route path=':id' element={<ChatWindow />} />
+                        </Route>
+                        <Route path='groups' element={<Groups />} >
+                            {/* Nested route relative to "/groups" */}
+                            <Route path=':id' element={<ChatWindow />} />
+                            <Route path='discover' element={<h1>discover tab</h1>} />
+                        </Route>
                         <Route path='setting' element={<Setting />} />
                     </Route>
                 </Routes>
