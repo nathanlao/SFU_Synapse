6ee742d002567ccd689f908a0080b0c41a3af1aa
nathanlao <gla93@sfu.ca>
2023-03-28 22:25:56 -0700

    sendMessage to emit a chat event to server

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams, useLocation } from 'react-router-dom';
+import io from 'socket.io-client'
 import { Typography, Box, Divider, Paper, InputBase, IconButton } from "@mui/material";
 import Avatar from '@mui/joy/Avatar';
 import SendIcon from '@mui/icons-material/Send';
@@ -8,7 +10,35 @@ import tempPic from "../../images/temp.png"
 
 import './Chat.css'
 
+const socket = io.connect("http://localhost:8080")
+
 export default function Chat() {
+
+    const { connectionId } = useParams()
+    // state value from sidepanel.jsx
+    const location = useLocation()
+    const userA_id = location?.state?.userAId
+    const userB_id = location?.state?.userBId
+    console.log(userA_id, "sending to " , userB_id)
+
+    const [message, setMessage] = useState("")
+
+    const handleInputChange = (e) => {
+        setMessage(e.target.value)
+    }
+
+    const sendMessage = (e) => {
+        e.preventDefault()
+
+        socket.emit('private-chat', {
+            sender_id: userA_id,
+            receiver_id: userB_id,
+            message: message,
+            timestamp: Date.now()
+        })
+        // Clear the input after sent
+        setMessage("")
+    }
 
     return (
         <>  
@@ -78,10 +108,12 @@ export default function Chat() {
             </div>
 
             <Divider />
-            <Paper component="form" className="input-container">
+            <Paper component="form" onSubmit={sendMessage} className="input-container">
                 <InputBase 
                     className="input-field"
-                    placeholder="Type a message">
+                    placeholder="Type a message"
+                    value={message}
+                    onChange={handleInputChange}>
                 </InputBase>
                 <IconButton type="submit">
                     <SendIcon className="send-button"/>
