04580ca8f5b27fa5b9fd490fc814a35e970fe706
nathanlao <gla93@sfu.ca>
2023-03-28 17:22:54 -0700

    Added pop up disconnect window

--- client/src/pages/Disconnect/Disconnect.jsx
+++ client/src/pages/Disconnect/Disconnect.jsx
@@ -1,16 +1,35 @@
-import React, { useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import { Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
+import { Button, IconButton, Dialog, DialogTitle, Divider,
+    DialogContent, Avatar, Card, CardHeader, Typography } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+
+import tempPic from "../../images/default_profile_picture.png"
+import arrow from "../../images/connected-arrows.png"
+
+import "./Disconnect.css"
 
 export default function Disconnect() {
-
     const { connectionId } = useParams()
+    const path = useLocation().pathname
     const nagivate = useNavigate()
 
+    const [openPopup, setOpenPopup] = useState(false)
+    useEffect(() => {
+        if(path === `/connections/${connectionId}/settings`) {
+            setOpenPopup(true)
+        }
+    },[connectionId, path])
+
+    function handlePopupClose() {
+        setOpenPopup(false);
+        nagivate(`/connections/${connectionId}`)
+    };
+    
     // DELETE request
     async function removeConnection() {
         try {
-            const response = await fetch(`/api/connections/${connectionId}/setting`, {method: "DELETE"})
+            const response = await fetch(`/api/connections/${connectionId}/settings`, {method: "DELETE"})
             if (!response.ok) {
                 // eslint-disable-next-line no-throw-literal
                 throw {
@@ -28,9 +47,52 @@ export default function Disconnect() {
     }
 
     return (
-        <>
-            <h1>UI page to confirm disconnection</h1>
-            <Button onClick={removeConnection}>Disconnect</Button>
-        </>
+        <Dialog open={openPopup} PaperProps={{
+            style: {
+                width: "600px",
+                borderRadius: "30px",
+                overflow: "hidden",
+            },
+        }}>
+            <DialogTitle className="pop-up-title-container">
+                Connection settings
+                <IconButton onClick={handlePopupClose}>
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
+            <Divider />
+            <DialogContent className="pop-up-content-container">
+                <div className="pop-up-content">
+                    <Card className="dialog-card">
+                        <CardHeader avatar={
+                            <Avatar alt="User A profile pic" src={tempPic} />
+                        }
+                        title="You"
+                        subheader="User A username"
+                    />
+                    </Card>
+                    <div className="inactive-arrow">
+                        <img alt="arrow" src={arrow}/>
+                        <Typography variant="body2" className="inactive">Inactive</Typography>
+                    </div>
+                    <Card className="dialog-card">
+                        <CardHeader avatar={
+                            <Avatar alt="User B profile pic"  src={tempPic} />
+                        }
+                        title="User B name"
+                        subheader="User B username"
+                    />
+                    </Card>
+                </div>
+                <div className="disconnect-prompt">
+                    <Typography variant="body2" component="div">
+                        Would you like to disconnect with Nathan?
+                    </Typography>
+                    <button onClick={removeConnection}>
+                        Disconnect
+                    </button>
+                </div>
+            </DialogContent>
+        </Dialog>
     )
 }
