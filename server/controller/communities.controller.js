caffe24b80b993e14b995fefa7e7edff636af2b1
Rachel Seong <dsa98@sfu.ca>
2023-03-30 12:29:31 -0700

    alter communities controller to use login session

--- server/controller/communities.controller.js
+++ server/controller/communities.controller.js
@@ -3,31 +3,30 @@ const { v4: uuidv4 } = require('uuid');
 const db = require('../db/connection.db').pool
 
 const createCommunity = (req, res) => {
-
-    const qSelect = "SELECT * FROM Groups WHERE group_name = ?";
     
-    db.query(qSelect, [req.body.community_name], (err,data) => {
-        
+    //get username of the community creator
+    if (!req.session || !req.session.user) {
+        return res.status(401).json("Login is required.")
+    }
+    const username = req.session.user.username
+
+    //check uniqueness of the name of the community being created
+    const qSelect = "SELECT * FROM Groups WHERE group_name = ?";
+    db.query(qSelect, [req.body.community_name], (err,data) => {    
         if (err) return res.status(500).json(err);
         if (data.length) return res.status(409).json("Community name is already used by another group. Please choose a different name.");
     })
-
-    const qCheckUserExists = "SELECT * FROM Users WHERE username = ?"
-
-    db.query(qCheckUserExists, [req.body.created_by], (err,data) => {
-        if (err) return res.status(500).json(err);
-        if (data.length === 0) return res.status(409).json("Requested owner is not an existing user.");
-    })
     
+    //add a group entry to db
     const group_id = uuidv4();
     const qInsertGroup = "INSERT INTO Groups (group_id, group_name, group_description) VALUE (?,?,?)";
 
     db.query(qInsertGroup, [group_id, req.body.community_name, req.body.bio], (err,data) => {
-       
         if (err) return res.status(500).json(err);
         qInsertCommunity = "INSERT INTO Communities (community_id, created_by, visibility) VALUE (?, ?, ?)";
-            
-        db.query(qInsertCommunity, [group_id, req.body.created_by, req.body.visibility], (err,data) => {
+        
+        //add a community entry, referencing the group entry above, to db
+        db.query(qInsertCommunity, [group_id, username, req.body.visibility], (err,data) => {
             if (err) return res.status(500).json(err);
             return res.status(200).json("Community created successfully.");
         })
