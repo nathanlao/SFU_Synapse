22b20a74494efb0089a1d9685023036f408e651c
nathanlao <gla93@sfu.ca>
2023-03-30 21:36:31 -0700

    Updated err handling

--- server/controller/socket-io.controller.js
+++ server/controller/socket-io.controller.js
@@ -14,14 +14,19 @@ const socketController = (io) => {
             }
 
             // Save the message to the database
-            const insertQuery = 'INSERT INTO DirectMessages SET ?';
-            db.query(insertQuery, newMessage, (err) => {
-                if (err) throw err;
-                console.log('Message saved to the database');
-            });
+            const insertQuery = 'INSERT INTO DirectMessages SET ?'
+            db.query(insertQuery, newMessage, (err, data) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).json("Internal server error")
+                } else {
+                    console.log('Message saved to the database')
+                    res.status(200).json(data)
+                }       
+            })
 
             // Emit the message to the receiver
-            socket.broadcast.to(receiver_id).emit('receiveMessage', newMessage);
+            socket.broadcast.to(receiver_id).emit('receiveMessage', newMessage)
         })
 
         socket.on("disconnect", () => {
