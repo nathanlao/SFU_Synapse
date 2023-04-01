5cfce9c2aae12233c1fb1072b16a6d0ed6ef64d9
nathanlao <gla93@sfu.ca>
2023-04-01 13:37:02 -0700

    update query to collect correct num members

--- server/controller/groups.controller.js
+++ server/controller/groups.controller.js
@@ -7,28 +7,50 @@ const getCourseGroups = (req, res) => {
     }
     const userId = req.session.user.user_id
 
-    const selectQuery = `SELECT M.group_id, G.group_name, G.photo, 
-                            COUNT(distinct M.user_id) AS num_members
+    const selectQuery = `SELECT M.group_id, G.group_name, G.photo
                         FROM MemberOf M, \`Groups\` G, Courses C 
                         WHERE M.group_id = G.group_id 
                         AND G.group_id = C.course_id
-                        AND M.user_id = ?
-                        GROUP BY M.group_id, G.group_name, G.photo`
+                        AND M.user_id = ?`
 
-    db.query(selectQuery, [userId], (err, data) => {
+    // Separate query to get number of members
+    const countMembersQuery = `SELECT M.group_id, COUNT(DISTINCT M.user_id) AS num_members
+                            FROM MemberOf M, \`Groups\` G, Courses C
+                            WHERE M.group_id = G.group_id
+                            AND G.group_id = C.course_id
+                            GROUP BY M.group_id`
+
+    db.query(selectQuery, [userId], (err, groupData) => {
         if (err) {
-            console.log(err)
-            res.status(500).json("Internal server error")
+            console.log(err);
+            res.status(500).json("Internal server error");
         } else {
-            if (data || data.length > 0) {
-                res.status(200).json(data)
-            } else {
-                console.log("No course groups found")
-                res.status(404).json("No course groups found")
-            }
+            db.query(countMembersQuery, (err, memberCountData) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).json("Internal server error");
+                } else {
+                    // combined groups and number of memebers data 
+                    const combinedData = groupData.map(group => {
+                        const memberCount = memberCountData.find(m => m.group_id === group.group_id)
+                        return {
+                            ...group,
+                            num_members: memberCount ? memberCount.num_members : 0,
+                        }
+                    })
+
+                    if (combinedData && combinedData.length > 0) {
+                        res.status(200).json(combinedData);
+                    } else {
+                        console.log("No course groups found")
+                        res.status(404).json("No course groups found")
+                    }
+                }
+            })
         }
-    })    
+    })
 }
+
 const createGroup = (req, res) => {
     res.send(`Received ${req.method} request to /groups`)
 }
